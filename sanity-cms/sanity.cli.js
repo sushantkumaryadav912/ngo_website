815a4e10e280a7f440d8ff77b01daedb732d35fc
Sushant <sushantkumaryadav912@gmail.com>
2025-09-17 00:19:21 +0530

    Fixed eisting issue with cms in phase 1

--- sanity-cms/sanity.cli.js
+++ sanity-cms/sanity.cli.js
@@ -2,12 +2,10 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'import.meta.env.VITE_SANITY_PROJECT_ID',
-    dataset: 'production'
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_DATASET || 'production'
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
+  deployment: {
+    autoUpdates: true
+  }
 })
